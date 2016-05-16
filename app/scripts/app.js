1ce7b7f29f109ebda60d85433472038843ee64c3
nattaniphac chootong <nattaniphac@nattaniphacs-MacBook-Air.local>
2016-05-16 18:36:44 +0700

    add manu UplondCart

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/uploadCart', {
+        templateUrl: 'views/uploadCart.html',
+        controller: 'uploadCartCtrl',
+        controllerAs: 'uploadCart'
+      })
       .otherwise({
         redirectTo: '/'
       });
