f1a6b2d19b034cf4e210aaf61d709f5933ecbf0e
Jakkrich Jongklangthawatchkij <toonelicious@gmail.com>
2016-05-17 23:31:19 +0700

    Edit index

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,59 +31,59 @@ angular
             })
 
         .when('/setupCart', {
-            templateUrl: 'views/setupCart.html',
-            controller: 'setupCartCtrl',
-            controllerAs: 'setupCart'
+                templateUrl: 'views/setupCart.html',
+                controller: 'setupCartCtrl',
+                controllerAs: 'setupCart'
 
-        })
-        .when('/blackList', {
-            templateUrl: 'views/blackList.html',
-            controller: 'blackListCtrl',
-            controllerAs: 'blackList'
+            })
+            .when('/blackList', {
+                templateUrl: 'views/blackList.html',
+                controller: 'blackListCtrl',
+                controllerAs: 'blackList'
 
-        })
-        .when('/promotionDiscount', {
-            templateUrl: 'views/promotionDiscount.html',
-            controller: 'promotionDiscountCtrl',
-            controllerAs: 'promotionDiscount'
+            })
+            .when('/promotionDiscount', {
+                templateUrl: 'views/promotionDiscount.html',
+                controller: 'promotionDiscountCtrl',
+                controllerAs: 'promotionDiscount'
 
-        })
-        .when('/comboProduct', {
-            templateUrl: 'views/comboProduct.html',
-            controller: 'comboProductCtrl',
-            controllerAs: 'comboProduct'
+            })
+            .when('/comboProduct', {
+                templateUrl: 'views/comboProduct.html',
+                controller: 'comboProductCtrl',
+                controllerAs: 'comboProduct'
 
-        })
-        .when('/exchangeMoney', {
-            templateUrl: 'views/exchangeMoney.html',
-            controller: 'exchangeMoneyCtrl',
-            controllerAs: 'exchangeMoney'
+            })
+            .when('/exchangeMoney', {
+                templateUrl: 'views/exchangeMoney.html',
+                controller: 'exchangeMoneyCtrl',
+                controllerAs: 'exchangeMoney'
 
-        })
-        .when('/dailyAmount', {
-            templateUrl: 'views/dailyAmount.html',
-            controller: 'dailyAmountCtrl',
-            controllerAs: 'dailyAmount'
+            })
+            .when('/dailyAmount', {
+                templateUrl: 'views/dailyAmount.html',
+                controller: 'dailyAmountCtrl',
+                controllerAs: 'dailyAmount'
 
-        })
-        .when('/dailyFlight', {
-            templateUrl: 'views/dailyFlight.html',
-            controller: 'dailyFlightCtrl',
-            controllerAs: 'dailyFlight'
+            })
+            .when('/dailyFlight', {
+                templateUrl: 'views/dailyFlight.html',
+                controller: 'dailyFlightCtrl',
+                controllerAs: 'dailyFlight'
 
-        })
-        .when('/dailyIncluded', {
-            templateUrl: 'views/dailyIncluded.html',
-            controller: 'dailyIncludedCtrl',
-            controllerAs: 'dailyIncluded'
+            })
+            .when('/dailyIncluded', {
+                templateUrl: 'views/dailyIncluded.html',
+                controller: 'dailyIncludedCtrl',
+                controllerAs: 'dailyIncluded'
 
-        })
-        .when('/dailyExcluded', {
-            templateUrl: 'views/dailyExcluded.html',
-            controller: 'dailyExcludedCtrl',
-            controllerAs: 'dailyExcluded'
+            })
+            .when('/dailyExcluded', {
+                templateUrl: 'views/dailyExcluded.html',
+                controller: 'dailyExcludedCtrl',
+                controllerAs: 'dailyExcluded'
 
-        })
+            })
 
         .when('/uploadCart', {
                 templateUrl: 'views/uploadCart.html',
@@ -94,4 +94,5 @@ angular
             .otherwise({
                 redirectTo: '/'
             });
+
     });
