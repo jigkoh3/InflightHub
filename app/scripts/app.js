1789ef8425eb1bb327b926717d37232fa5f12e45
Jakkrich Jongklangthawatchkij <toonelicious@gmail.com>
2016-05-17 18:39:55 +0700

    Menu complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,6 +36,55 @@ angular
             controllerAs: 'setupCart'
 
         })
+        .when('/blackList', {
+            templateUrl: 'views/blackList.html',
+            controller: 'blackListCtrl',
+            controllerAs: 'blackList'
+
+        })
+        .when('/promotionDiscount', {
+            templateUrl: 'views/promotionDiscount.html',
+            controller: 'promotionDiscountCtrl',
+            controllerAs: 'promotionDiscount'
+
+        })
+        .when('/comboProduct', {
+            templateUrl: 'views/comboProduct.html',
+            controller: 'comboProductCtrl',
+            controllerAs: 'comboProduct'
+
+        })
+        .when('/exchangeMoney', {
+            templateUrl: 'views/exchangeMoney.html',
+            controller: 'exchangeMoneyCtrl',
+            controllerAs: 'exchangeMoney'
+
+        })
+        .when('/dailyAmount', {
+            templateUrl: 'views/dailyAmount.html',
+            controller: 'dailyAmountCtrl',
+            controllerAs: 'dailyAmount'
+
+        })
+        .when('/dailyFlight', {
+            templateUrl: 'views/dailyFlight.html',
+            controller: 'dailyFlightCtrl',
+            controllerAs: 'dailyFlight'
+
+        })
+        .when('/dailyIncluded', {
+            templateUrl: 'views/dailyIncluded.html',
+            controller: 'dailyIncludedCtrl',
+            controllerAs: 'dailyIncluded'
+
+        })
+        .when('/dailyExcluded', {
+            templateUrl: 'views/dailyExcluded.html',
+            controller: 'dailyExcludedCtrl',
+            controllerAs: 'dailyExcluded'
+
+        })
+
 
 
         .when('/uploadCart', {
